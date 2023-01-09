36818b0c19f8bb5ab3280056a2c06f9eb34aac20
ortub10 <ortub10@gmail.com>
2023-01-09 23:07:14 +0200

    params

--- express/routes/shop.js
+++ express/routes/shop.js
@@ -6,6 +6,15 @@ router.get("/", (req, res) => {
   res.json(prods_ar);
 });
 
+router.get("/:id", (req, res) => {
+  let idP = req.params.id;
+  let prod = prods_ar.find((item) => {
+    return item.id == idP;
+  });
+  res.json(prod);
+});
+
+//with query string
 router.get("/catagory", (req, res) => {
   let catagoryQ = req.query.catagory;
   let temp_ar = prods_ar.filter((item) => {
@@ -14,4 +23,23 @@ router.get("/catagory", (req, res) => {
   res.json(temp_ar);
 });
 
+//with params
+router.get("/catagory/:catagoryName", (req, res) => {
+  let catagoryP = req.params.catagoryName;
+  let temp_ar = prods_ar.filter((item) => {
+    return item.cat == catagoryP;
+  });
+  res.json(temp_ar);
+});
+
+//------------------------------
+
+//?min
+router.get("/query", (req, res) => {
+  let min = req.query.min;
+  let temp_ar = prods_ar.filter((item) => {
+    return Number(item.price) > min;
+  });
+  res.json(temp_ar);
+});
 module.exports = router;
