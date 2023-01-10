bf6992732491cf8232c1507efd5ea9eb85e8db30
ortub10 <ortub10@gmail.com>
2023-01-10 19:23:28 +0200

    Unique email, encrypted password

--- express_with_mongo/routes/users.js
+++ express_with_mongo/routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { UserModel, validUser } = require("../models/userModel");
 const router = express.Router();
 
@@ -11,9 +12,19 @@ router.post("/", async (req, res) => {
   if (validBody.error) {
     return res.status(400).json(validBody.error.details);
   }
-  let user = new UserModel(req.body);
-  await user.save();
-  res.json(user);
+
+  try {
+    let user = new UserModel(req.body);
+    user.password = await bcrypt.hash(user.password, 10);
+    await user.save();
+    user.password = "******";
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .json({ err: "Email already in system or there another problem" });
+  }
 });
 
 module.exports = router;
