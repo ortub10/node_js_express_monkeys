562e264ee391abfc2f0061ab867438fbf012d626
ortub10 <ortub10@gmail.com>
2023-01-10 17:37:55 +0200

    delete and put

--- express_with_mongo/routes/foods.js
+++ express_with_mongo/routes/foods.js
@@ -17,4 +17,27 @@ router.post("/", async (req, res) => {
   res.json(food);
 });
 
+router.delete("/:idDel", async (req, res) => {
+  try {
+    let data = await FoodModel.deleteOne({ _id: req.params.idDel });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+});
+
+router.put("/:idEdit", async (req, res) => {
+  let validBody = validFood(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let data = await FoodModel.updateOne({ _id: req.params.idEdit }, req.body);
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+});
 module.exports = router;
