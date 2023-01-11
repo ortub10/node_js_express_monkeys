b84d2925120b4c6b6d0e1b85c76a70bd3b62afe1
ortub10 <ortub10@gmail.com>
2023-01-11 15:14:17 +0200

    post request from client

--- client_side_connection/client/js/listFoods.js
+++ client_side_connection/client/js/listFoods.js
@@ -14,19 +14,49 @@ const declareViewEvents = () => {
       img: document.querySelector("#id_img").value,
     };
 
-    console.log(objBody);
+    if (objBody.name.length < 2) {
+      return alert("Name too short");
+    } else if (objBody.cal < 1) {
+      return alert("Enter valid calories");
+    } else if (objBody.price < 1) {
+      return alert("Enter valid price");
+    }
+    doPostApi(objBody);
   });
 };
 
-const doApi = async () => {
-  //Problem cors
+const doPostApi = async (_body) => {
   const url = "http://localhost:3020/foods";
-  let resp = await axios.get(url);
-  console.log(resp.data);
-  createFoodList(resp.data);
+  try {
+    let resp = await axios({
+      url,
+      method: "POST",
+      data: _body,
+    });
+
+    console.log(resp.data);
+    if (resp.data._id) {
+      doApi();
+    }
+  } catch (err) {
+    console.log(err);
+    alert("There is problem try again later");
+  }
+};
+const doApi = async () => {
+  try {
+    const url = "http://localhost:3020/foods";
+    let resp = await axios.get(url);
+    console.log(resp.data);
+    createFoodList(resp.data);
+  } catch (err) {
+    console.log(err);
+    alert("There is problem try again later");
+  }
 };
 
 const createFoodList = (_ar) => {
+  document.querySelector("#id_list").innerHTML = "";
   _ar.forEach((item) => {
     const li = document.createElement("li");
     li.innerHTML = `<li>${item.name} - ${item.price} nis</li>`;
